86f5cfb956e74eed087ae89d31f4ae76568dab69
Miguel-Estradam <miguelestradam12@gmail.com>
2023-02-12 23:15:31 -0500

    terminate Backend

--- venv/backend/api/models.py
+++ venv/backend/api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    body = models.CharField(max_length=50)
--- venv/backend/api/views.py
+++ venv/backend/api/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import Blog
+from .serializers import BlogSerializer
+
+
+@api_view(['GET'])
+def getBlogs(request):
+    blog = Blog.objects.all()
+    serializer = BlogSerializer(blog, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def postBlog(request):
+    data = request.data
+    blog = Blog.objects.create(
+        body= data['body']
+    )
+    serializer = BlogSerializer(blog, many=False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def putBlog(request,pk):
+    data = request.data
+    blog = Blog.objects.get(id = pk)
+    Serializer = BlogSerializer(instance=blog, data = data)
+    if Serializer.is_valid():
+        Serializer.save()
+        return Response(Serializer.data)
+
+@api_view(['DELETE'])
+def deleteBlog(request,pk):
+    blog = Blog.objects.get(id=pk)
+    blog.delete()
+    return Response('Blog Eliminado')
+
